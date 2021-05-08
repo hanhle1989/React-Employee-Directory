34869bc98c4dfdd3f04a00bad9cf6065f561c41b
Hanh Le <hanhdtle@gmail.com>
2021-05-08 11:31:01 -0700

    added EmployeeTable and Search components

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -3,7 +3,17 @@ import React from "react";
 
 function EmployeeTable(props) {
     return (
-        <div></div>
+        <tbody>
+            {props.results.filter(input => input.name.first.toLowerCase().includes(props.filter)).map(item => (
+                < tr key={item.login.uuid} >
+                    <td className="p-4"><img src={item.picture.medium} alt="employee" className="circle" /></td>
+                    <td className="px-4">{item.name.first} {item.name.last}</td>
+                    <td className="px-4"><b>Email:</b> {item.email}</td>
+                    <td className="px-4"><b>Phone:</b>{item.phone}</td>
+                </tr >
+            ))
+            }
+        </tbody >
     );
 }
 
